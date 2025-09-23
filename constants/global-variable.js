84e3497b87af2a2e02706806cf7d8cba4dd64b9f
Johan <johan2a409@gmail.com>
2025-09-22 19:35:28 -0500

    Conexion con vercel

--- constants/global-variable.js
+++ constants/global-variable.js
@@ -1,6 +1,6 @@
 // Usar variable de entorno si está disponible, sino usar la URL por defecto
 const baseUrl =
-  import.meta.env.VITE_API_URL ||
+  import.meta.env.REACT_APP_API_URL ||
   "https://backend-production-33a8.up.railway.app/api/employee";
 
 export { baseUrl };
